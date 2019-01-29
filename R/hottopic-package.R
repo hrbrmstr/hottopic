00f58dc94c80da5a974841dcad3152eea516489e
hrbrmstr <bob@rud.is>
2019-01-29 16:10:15 -0500

    R package repo initialization complete

---
+++ R/hottopic-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/hottopic>
+#' - BugReports: <https://gitlab.com/hrbrmstr/hottopic/issues>
+#' 
+#' @md
+#' @name hottopic
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
